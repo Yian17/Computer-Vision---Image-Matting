c29291eb26917475b393a62cb12b5dc4f9a1fc7d
Yian17 <32120649+Yian17@users.noreply.github.com>
2020-07-22 17:23:57 -0400

    Add files via upload

---
+++ project/viscomp.py
@@ -0,0 +1,21 @@
+#! /usr/local/bin/python
+
+## CSC320 Winter 2017 
+## Assignment 1
+## (c) Kyros Kutulakos
+##
+## DISTRIBUTION OF THIS CODE ANY FORM (ELECTRONIC OR OTHERWISE)
+## WITHOUT WRITTEN AUTHORIZATION OF THE INSTRUCTOR IS STRICTLY 
+## PROHIBITED. VIOLATION OF THIS POLICY WILL BE CONSIDERED AN
+## ACT OF ACADEMIC DISHONESTY
+
+##
+## DO NOT MODIFY ANY PART OF THIS FILE
+##
+
+import sys
+from matting import run
+
+if __name__ == '__main__':
+    run.main(sys.argv)
+
